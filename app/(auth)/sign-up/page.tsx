374b8752e0a09907b1dda64a652aab23a3718be5
Akaikenlol <keithken972@gmail.com>
2023-11-22 23:13:17 +0630

    auth, folder structure and global css

--- app/(auth)/sign-up/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function SignUp() {
-	return (
-		<main className="flex justify-center items-center text-center min-h-screen">
-			<h1>Sign Up Page</h1>
-		</main>
-	);
-}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+		},
+	},
+	fontFamily: {
+		inter: ["var(--font-inter)"],
+		spaceGrotesk: ["var(--font-spaceGrotesk)"],
+	},
+
+	plugins: [],
+};
+export default config;
